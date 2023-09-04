c0263ea10b189805028f1201d1fb68d6311bf6c8
ozgeonline <nisroc.414@hotmail.com>
2023-09-04 17:19:35 +0300

    publish website

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ function Header() {
   return (
       <header className="header">
           <img 
-              src="./images/troll-face.png" 
+              src="./images/TrollFace.png" 
               className="header--image"
               alt="header-img"
           />
           <h2 className="header--title">Meme Generator</h2>
-          <h4 className="header--project">React Course - Project 3</h4>
+          <h4 className="header--project">Project 3</h4>
       </header>
   )
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,20 +1,19 @@
 import React from "react";
-// import memesData from "../memesData";
 
 function Meme(){
 
-  // const [memeImage,setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
   const [meme,setMeme] = React.useState({
     topText:"",
     bottomText:"",
     randomImage:"http://i.imgflip.com/1bij.jpg"
   })
+
   const [allMemes,setAllMemes] = React.useState()
 
   React.useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(data => setAllMemes(data.data.memes))
+    .then(res => res.json())
+    .then(data => setAllMemes(data.data.memes))
 }, [])
 
 // console.log(allMemes)
